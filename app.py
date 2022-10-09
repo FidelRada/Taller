9eac7cdada36b2a3982f27564d5fc94f27b68545
fidelrada <fidel.rada.bo@gmail.com>
2022-10-09 12:56:04 -0400

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template_string("yolo");
 
 if __name__ == '__main__':
     socketio.run(app)
